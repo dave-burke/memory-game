47487d11e41bb0823bf8403751e19467d6c218a5
Dave Burke <dburke84@gmail.com>
2021-11-20 10:52:07 -0600

    Add basic memory game

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,48 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Memory Game</h1>
+<p>
+    Show numbers for: <input id="timeoutSlider" bind:value={timeout} type="range" min="100" max="10000" step="100"/>
+    {timeout}ms
+</p>
+<button on:click={handleGoClick}>Go</button>
+{#if showTable}
+<table>
+    <tbody>
+        <tr>
+            <td>{numbers[0]}</td>
+            <td>{numbers[1]}</td>
+            <td>{numbers[2]}</td>
+            <td>{numbers[3]}</td>
+        </tr>
+    </tbody>
+</table>
+{/if}
+
+<script>
+    let numbers = [0, 0, 0, 0]
+    let timeout = 500
+    let showTable = false
+    function generateRandomNumber(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min
+    }
+    function handleGoClick() {
+        for(let i = 0; i < numbers.length; i++){
+            numbers[i] = generateRandomNumber(0,9)
+            showTable = true
+            setTimeout(() => showTable = false, timeout)
+        }
+    }
+</script>
+<style>
+    #timeoutSlider {
+        width: 20rem;
+    }
+    table {
+        margin-left: auto;
+        margin-right: auto;
+    }
+    table td {
+        padding: 2rem;
+        font-size: xx-large;
+        font-weight: bold;
+    }
+</style>

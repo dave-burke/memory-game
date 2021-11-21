27c89e72c70dede2ad6e167bca3d523810798af1
Dave Burke <dburke84@gmail.com>
2021-11-21 15:40:00 -0600

    Allow rotation and translation

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,19 @@
 <script type="typescript">
 	let numbers: Array<number> = [0, 0, 0, 0];
-	let timeout: number = 1000;
 	let showNumbers: boolean = false;
-	let alwaysShowNumbers: boolean = false;
+
+	let timeout: number = 1000;
 	let timeoutSeconds: number;
 	$: timeoutSeconds = Number((timeout / 1000).toFixed(1));
+
+	let rotate: boolean = false;
+	let rotation: number = 0;
+	let move: boolean = false;
+	let xPos: number = 0;
+	let yPos: number = 0;
+
+	let alwaysShowNumbers: boolean = false;
+
 	function generateRandomNumber(min: number, max: number): number {
 		return Math.floor(Math.random() * (max - min + 1)) + min;
 	}
@@ -12,6 +21,9 @@
 		for (let i = 0; i < numbers.length; i++) {
 			numbers[i] = generateRandomNumber(0, 9);
 			showNumbers = true;
+			rotation = rotate ? generateRandomNumber(-45, 45) : 0;
+			xPos = move ? generateRandomNumber(-5, 5) : 0;
+			yPos = move ? generateRandomNumber(-5, 5) : 0;
 			setTimeout(() => (showNumbers = false), timeout);
 		}
 	}
@@ -22,20 +34,27 @@
 		<h1>Memory Game</h1>
 	</header>
 	<section class="controls">
-		Show numbers for: <input
-			id="timeoutSlider"
-			bind:value={timeout}
-			type="range"
-			min="500"
-			max="5000"
-			step="500"
-		/>
-		{timeoutSeconds} seconds
-		<input type="checkbox" bind:checked="{alwaysShowNumbers}"/> always show numbers
+		<p>
+			<span class="nowrap">Show numbers for:</span>
+			<input
+				id="timeoutSlider"
+				bind:value={timeout}
+				type="range"
+				min="500"
+				max="5000"
+				step="500"
+			/>
+			<span class="nowrap">{timeoutSeconds} seconds</span>
+		</p>
+		<p>
+			<input type="checkbox" bind:checked="{rotate}"/> rotate
+			<input type="checkbox" bind:checked="{move}"/> move
+			<input type="checkbox" bind:checked="{alwaysShowNumbers}"/> always show numbers
+		</p>
 	</section>
 	<section class="numbers">
 		{#if showNumbers || alwaysShowNumbers}
-			<div id="numberValue">
+			<div id="numberValue" style="transform: rotate({rotation}deg) translateX({xPos}rem) translateY({yPos}rem)">
 				{numbers[0]}
 				{numbers[1]}
 				{numbers[2]}
@@ -86,5 +105,9 @@
 		padding: 2rem;
 		font-size: larger;
 		font-weight: bold;
+	}
+
+	.nowrap {
+		white-space: nowrap;
 	}
 </style>
